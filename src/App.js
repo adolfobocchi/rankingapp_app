381a0561442a0c81dbb3f2e1ec1fe24be40a306c
ADOLFO BOCCHI <adolfo.bocchi@senacsp.edu.br>
2023-11-21 13:37:59 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="ranking-area">
+      <div className="ranking-header">
+        <h2>RANKING</h2>
+      </div>
+      <div className="ranking-list">
+        {/* vai vir da api la do banco de dados */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
