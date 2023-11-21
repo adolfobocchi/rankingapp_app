500f211e1b8b218f7f3ff61a6b4044be656f55ff
ADOLFO BOCCHI <adolfo.bocchi@senacsp.edu.br>
2023-11-21 16:49:58 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import api from './api';
 
 function App() {
+  const [rankingData, setRankingData] = useState([]);
+
+  useEffect(()=> {
+    const fetchData = async () => {
+      try {
+        const response = await api.get('/ranking');
+        setRankingData(response.data);
+      } catch (error) {
+        console.error('erro ao buscar dados na api: ', error);
+      }
+    };
+
+    fetchData();
+  }, [])
+
   return (
+    <>
+    <h1>2ª TURMA DE JOGOS - THE BEST</h1>
     <div className="ranking-area">
       <div className="ranking-header">
         <h2>RANKING</h2>
       </div>
       <div className="ranking-list">
-        {/* vai vir da api la do banco de dados */}
+        {/* {JSON.stringify(rankingData)} */}
+        <ul>
+          {rankingData.map((item, index) => (
+            <li className='ranking-item'  key={index}>
+              <div className="nick">
+                {item.nick}
+              </div>
+              <div className="score">
+                {item.score}
+              </div>
+            
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
+    </>
   );
 }
 
